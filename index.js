f08b86c0ad8f5e1d8689b9b5960ca5f35e57c9e1
shravan20 <mrshravankumarb@gmail.com>
2021-11-04 22:28:50 +0530

    [express as devdep] feat  | added cli

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const app = require("./port-logic");
+const logger = require("console");
+/**
+ * To create CLI interface
+ */
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
 
-app(8081);
+readline.question("Enter the port number(mandatory)", port => {
+    if(!port) 
+        throw new Error("Empty Port is not allowed");
+
+    readline.question("Enter the signal(optional):", signal => {
+        readline.close();
+        app(port,signal);
+    });
+});
+
