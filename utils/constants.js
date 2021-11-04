7bd8d518ab54bd8cede10fc24b490a44c21e609d
shravan20 <mrshravankumarb@gmail.com>
2021-11-04 21:48:24 +0530

    [utils]  | Added constant file

---
+++ utils/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    OPERATING_SYSTEM_LABELS : ["darwin", "aix", "freebsd", "linux", "openbsd", "sunos", "win32", "android"],
+    INTERRUPT_TYPE : {
+        SIGHUP: 1, // Hangup
+        SIGINT: 2, // Interrupt from keyboard
+        SIGKILL: 9, // Kill Signal (Default)
+        SIGTERM: 15, // Termination Signal
+        SIGSTOP: 19, // Stop the process
+    }
+};
